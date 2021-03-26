265cd2bb8403946bc6e0b98bc66de67401563f68
sam <skb502@nyu.edu>
2021-03-25 21:28:53 -0400

    initial commit of in-progress game - fish scripts/prefabs connected

---
+++ Assets/Scripts/Fish.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+    public Vector3 destination;
+
+    // Swims towards a destination
+    public void Swim()
+    {
+        Vector3 swimVector = destination - transform.position;
+        transform.position += swimVector.normalized * Time.deltaTime;
+        transform.up = swimVector.normalized;
+
+        if (Vector3.Distance(destination, transform.position) < 0.1f) {
+            destination = transform.position;
+            transform.rotation = (Quaternion.Euler(0, 0, 90));
+        }
+
+        Debug.DrawLine(transform.position, destination, Color.yellow);
+    }
+}
---
+++ Assets/Scripts/FishGod.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishGod : MonoBehaviour
+{
+    public List<Fish> fishPrefabs = new List<Fish>(); 
+    List<Fish> fishList = new List<Fish>();
+
+    void Start() 
+    {
+        
+    }
+
+    void Update()
+    {
+        // If I CLICK, spawn a new fish.
+
+        // If I press SPACEBAR, tell all fish to randomly swim somewhere else.
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            foreach (Fish fish in fishList)
+                fish.destination = getRandomPos();
+        }
+    }
+    public Color makeColor (float r, float g, float b) => new Color (r/255, g/255, b/255);
+    private Vector2 getRandomPos() => new Vector2(Random.Range(-7f, 7f), Random.Range(-4f, 4f));
+}
